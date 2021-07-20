22168c3b9104e0222c8e74056bcf1389fa400242
Victor Hugo <vhclaudiogomes@gmail.com>
2021-07-20 19:04:23 -0300

    LOGINPAGE | Adicionado o front-end da pagina de login

---
+++ lib/screens/login_page.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../widgets/Login_passwordBuilder.dart';
+import '../widgets/Login_loginButtonBuilder.dart';
+import '../widgets/Login_emailBuilder.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: Stack(
+          children: <Widget>[
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.center,
+                      //Nessa parte queria colocar um gradient, porém eu sou pessimo para cores e não faço a minima ideia do que colocar
+                      //Deixarei passando por aqui a gente
+                      colors: [
+                    Color(0xff268C82),
+                    Color(0xff268C82),
+                    Color(0xff268C82),
+                    Color(0xff268C82),
+                  ])),
+              child: SingleChildScrollView(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 400,
+                  vertical: 120,
+                ),
+                child: Container(
+                  height: 400,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    color: Color(0xff2A3659),
+                    border: Border.all(
+                      color: Color(0xff2A3659),
+                    ),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'Login',
+                        style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      emailBuilder(),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      passwordBuilder(),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      loginButtonBuilder()
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/Login_emailBuilder.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+Widget emailBuilder() {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        'Email',
+        style: TextStyle(
+            color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+      ),
+      SizedBox(
+        height: 10,
+      ),
+      Container(
+        alignment: Alignment.centerLeft,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 6,
+                offset: Offset(0, 2),
+              )
+            ]),
+        height: 60,
+        width: 500,
+        child: TextField(
+          keyboardType: TextInputType.emailAddress,
+          style: TextStyle(color: Colors.black87),
+          decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14),
+              prefixIcon: Icon(
+                Icons.email,
+                color: Colors.black,
+              ),
+              hintText: 'Email',
+              hintStyle: TextStyle(
+                color: Colors.black38,
+              )),
+        ),
+      ),
+    ],
+  );
+}
