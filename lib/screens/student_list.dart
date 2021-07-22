750f6b448c89c808e8a5d5491a2d7698c95ef5d8
Thaís <thais.calixto@mail.uft.edu.br>
2021-07-22 14:56:31 -0300

    support/file_fixes | Correção de nomenclaturas em student_item
    - Renomeei alguns arquivos para snake_case
    - Apaguei o arquivo `lib/widgets/Student_item.dart` que não estava sendo usado

--- lib/screens/student_list.dart
+++ lib/screens/student_list.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:project/models/student.dart';
+import 'package:project/models/student_model.dart';
 import 'package:project/theme/navbar_theme.dart';
-import 'package:project/widgets/Student_item_list.dart';
+import 'package:project/widgets/student_item.dart';
 import 'package:project/widgets/navbar/drawer.dart';
 
 class StudentList extends StatelessWidget {
