4f166120e3b356b2bb403695cf86be6abca20c9e
Thaís <thais.calixto@mail.uft.edu.br>
2021-07-25 20:37:50 -0300

    feature/firebase_auth | Formatação de código
    - Adicionei mais uma virgula e dei ctrl+s e o arquivo login_page.dart formatou automaticamente
    - Removi o texto Login que estava sendo exibido atrás do ícone em inputs_builder.dart

--- lib/widgets/login/inputs_builder.dart
+++ lib/widgets/login/inputs_builder.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class InputLoginPage extends StatelessWidget {
   final bool hideText;
   final String hintText;
@@ -9,52 +10,54 @@ class InputLoginPage extends StatelessWidget {
     required this.hintText,
     required this.icon,
   });
-  
+
   @override
   Widget build(BuildContext context) {
     return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: <Widget>[
-      Text(
-        hintText,
-        style: TextStyle(
-            color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
-      ),
-      SizedBox(
-        height: 10,
-      ),
-      Container(
-        alignment: Alignment.centerLeft,
-        decoration: BoxDecoration(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          hintText,
+          style: TextStyle(
             color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black26,
-                blurRadius: 6,
-                offset: Offset(0, 2),
-              )
-            ]),
-        height: 60,
-        width: 500,
-        child: TextField(
-          obscureText: hideText,
-          style: TextStyle(color: Colors.black87),
-          decoration: InputDecoration(
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.only(top: 14),
-              hintText: hintText,
-              prefixIcon: Icon(
-                icon,
-                color: Colors.black,
-              ),
-              hintStyle: TextStyle(
-                color: Colors.black38,
-              )),
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
         ),
-      ),
-    ],
-  );
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 6,
+                  offset: Offset(0, 2),
+                )
+              ]),
+          height: 60,
+          width: 500,
+          child: TextField(
+            obscureText: hideText,
+            style: TextStyle(color: Colors.black87),
+            decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(top: 14),
+                hintText: hintText,
+                prefixIcon: Icon(
+                  icon,
+                  color: Colors.black,
+                ),
+                hintStyle: TextStyle(
+                  color: Colors.black38,
+                )),
+          ),
+        ),
+      ],
+    );
   }
 }
-
