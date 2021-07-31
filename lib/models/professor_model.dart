37b1ebc956827da7ae1047f3a12cc0a07d231ec3
Victor Hugo <vhclaudiogomes@gmail.com>
2021-07-31 18:25:43 -0300

    feature/models-professor | Adicionado o modelo da classe professor

---
+++ lib/models/professor_model.dart
@@ -0,0 +1,13 @@
+class Professor {
+  final String professorName;
+  final String email;
+  final String password;
+  final String idUnicoProfessor;
+  
+  Professor({
+    required this.idUnicoProfessor,
+    required this.professorName,
+    required this.email,
+    required this.password,
+  });
+}
