368cba20f174ad038687096c34abdce1ce96801a
Victor Hugo <vhclaudiogomes@gmail.com>
2021-07-31 21:11:13 -0300

    feature/refactor_models | Adicionado a abstração da classe usuario.

---
+++ lib/models/user.dart
@@ -0,0 +1,15 @@
+class User {
+  final String name;
+  final String email;
+  final String senha;
+  final String uniqueID;
+  final bool isStudent;
+
+  User({
+    required this.name,
+    required this.email,
+    required this.senha,
+    required this.uniqueID,
+    required this.isStudent,
+  });
+}
