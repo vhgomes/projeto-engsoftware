09fc54a9c087cb4974d25cec929a98d41279f5d4
Thaís <thais.calixto@mail.uft.edu.br>
2021-08-09 14:31:56 -0300

    feature/student_grade | Conexão com o FirebaseFirestore
    - Conectei com o banco de dados
    - Li os documentos presentes na coleção 'student'
    - Removi a lista de alunos 'loadedStudents'
    - Removi a importação de pacotes inúteis

--- lib/screens/professor/student_grade.dart
+++ lib/screens/professor/student_grade.dart
@@ -1,44 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:project/models/user.dart';
 import 'package:project/theme/app_theme.dart';
 import 'package:project/theme/navbar_theme.dart';
-import 'package:project/models/student_model.dart';
 import 'package:project/widgets/student_grade.dart';
 import 'package:project/widgets/navbar/professor_drawer.dart';
 
 class StudentListGrade extends StatelessWidget {
-  final List<Student> loadedStudents = [
-    Student(
-      User(
-        password: '123',
-        name: 'Filipe Marques de Souza',
-        isStudent: true,
-        uniqueID: 's1',
-        email: 'filipe.souza1906@gmail.com',
-      ),
-      nota1: 7.1,
-      nota2: 8.5,
-    ),
-  ];
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: drawerBGClor,
       ),
       drawer: ProfessorDrawer(),
-      body: Container(
-        decoration: AppTheme.backgroudTheme(),
-        child: ListView.builder(
-          padding: const EdgeInsets.all(10.0),
-          itemCount: loadedStudents.length,
-          itemBuilder: (context, index) => Studentgrade(
-            name: loadedStudents[index].name,
-            nota1: loadedStudents[index].nota1,
-            nota2: loadedStudents[index].nota2,
-            media:
-                (loadedStudents[index].nota1 + loadedStudents[index].nota2) / 2,
-          ),
-        ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('student').snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+          final documents = streamSnapshot.data!.docs;
+
+          return Container(
+            decoration: AppTheme.backgroudTheme(),
+            child: ListView.builder(
+                padding: const EdgeInsets.all(10.0),
+                itemCount: documents.length,
+                itemBuilder: (context, index) => Container(
+                      child: Studentgrade(
+                        name: documents[index]['name'],
+                        nota1: 0,
+                        nota2: 0,
+                        media: 0,
+                      ),
+                    )),
+          );
+        },
       ),
     );
   }
