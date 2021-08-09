8918d78db6c0985b88b53af7ca4c3cc391d2129d
Thaís <thais.calixto@mail.uft.edu.br>
2021-08-09 15:42:37 -0300

    feature/refactor_grades | Correção de conflitos
    - Atualizei esta branch com a develop, fazendo um merge
    - Mudei a cor do ícone da navbar em 'singup_form.dart'
    - Removi importação de pacote inútil em 'student_singup.dart'

--- lib/widgets/auth/singup_form.dart
+++ lib/widgets/auth/singup_form.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:project/theme/app_theme.dart';
